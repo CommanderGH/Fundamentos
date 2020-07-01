0a7778282f07951a68ea3608c1de008f2c0f2f00
Commander <danigare29@gmail.com>
2020-07-01 12:17:58 -0500

    Implementación de SASS y JavaScript

---
+++ Java Script/Fundamentos/index.js
@@ -0,0 +1,17 @@
+console.log("Hola Mundo")
+
+var array = [
+    ['c',false,"neutral",-0.33,'m'],
+    [1.2,'a',-0.1,'a',"uno"],
+    [true,3,'s',0.1,"2.3"],
+    ["hola",'a',0.333,'a',-1],
+    ['s',false,"caballo",10,'s']
+]
+
+console.log(array[0][0]," "," "," ",array[0][4])
+console.log(" ",array[1][1]," ",array[1][3]," ")
+console.log(" "," ",array[2][2]," "," ")
+console.log(" ",array[3][1]," ",array[3][3]," ")
+console.log(array[4][0]," "," "," ",array[4][4])
+
+console.log("\n",array)
